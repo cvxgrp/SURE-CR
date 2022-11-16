eb87453ecb85acd906bc3cb407f81a901bb36064
Thistleman <codes5096@gmail.com>
2022-11-16 11:36:56 -0800

    added CI/CD Files and tailored implementation

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python
+import subprocess
 from setuptools import setup
 
 with open("README.md", "r") as fh:
    long_description = fh.read()
 
+# get all the git tags from the cmd line that follow our versioning pattern
+git_tags = subprocess.Popen(
+    ["git", "tag", "--list", "v*[0-9]", "--sort=version:refname"],
+    stdout=subprocess.PIPE,
+)
+tags = git_tags.stdout.read()
+git_tags.stdout.close()
+tags = tags.decode("utf-8").split("\n")
+tags.sort()
+
+# PEP 440 won't accept the v in front, so here we remove it, strip the new line and decode the byte stream
+VERSION_FROM_GIT_TAG = tags[-1][1:]
+
 setup(
     name="surecr",
+    version=VERSION_FROM_GIT_TAG,  # Required
     setup_requires=["setuptools>=18.0"],
     install_requires=[
         "numpy >= 1.17.5",
@@ -15,6 +30,9 @@ setup(
         "cvxpylayers",
         "torch-linops",
     ],
+    description="This package enables tractable evaluation of Stein's Unbiased Risk Estimate on convexly regularized estimators.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/cvxgrp/SURE-CR",
     classifiers=[
         "Programming Language :: Python :: 3",
